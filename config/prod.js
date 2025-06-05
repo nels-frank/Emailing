5276ec3cb02a303a4cdfe52aafbe2a82ad605da7
nels-frank <frankadzua@gmail.com>
2025-06-05 06:51:07 -0700

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,16 @@
+//prod.js - production keys here!!
+module.exports = {
+googleClientID: process.env.GOOGLE_CLIENT_ID,
+googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+mongoURI: process.env.MONGO_URI,
+expressKey: process.env.EXPRESS_KEY
+};
+
+
+//test db username and password:username:frankadzua, password: jfvSDfCGEz05JAmf
+
+
+//mongodb+srv://frankadzua:<db_password>@emailing-prod-cluster.e5e1rry.mongodb.net/?retryWrites=true&w=majority&appName=Emailing-prod-Cluster
+
+
+//connection string for test db: mongodb+srv://frankadzua:jfvSDfCGEz05JAmf@cluster0.tyut7ps.mongodb.net/emailingprod?retryWrites=true&w=majority&appName=Cluster0
