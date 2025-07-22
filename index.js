1582038ec4c93dfb1486a9302eb2549f7d4d0afa
nels-frank <frankadzua@gmail.com>
2025-07-22 06:25:04 -0700

    Refix

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
   //})
 //);
 
-app.set('trust proxy', 1); // trust first proxy
+//-app.set('trust proxy', 1); // trust first proxy
 
 app.use(
   session({
@@ -51,18 +51,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);  
 require('./routes/billingRoutes')(app);
 
-if (process.env.NODE_ENV === 'production') {
+//-if (process.env.NODE_ENV === 'production') {
   //Express will save up production assets
   //like our main.js file, main.css file!
-  app.use(express.static('client/build'));
+ //- app.use(express.static('client/build'));
 
   //Express will serve up the index.html file
   //If it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+  //-const path = require('path');
+  //-app.get('*', (req, res) => {
+   //- res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  //-})
+//-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
