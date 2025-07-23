a0e7095054f37c5a5c2b8aab4c47b4b48a5ec7bc
nels-frank <frankadzua@gmail.com>
2025-07-23 10:35:38 -0700

    working on index

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-//-app.set('trust proxy', 1); // trust first proxy
-
+app.set('trust proxy', 1); // trust first proxy
 app.use(
   session({
     secret: keys.expressKey,
@@ -34,18 +33,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);  
 require('./routes/billingRoutes')(app);
 
-//-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   //Express will save up production assets
   //like our main.js file, main.css file!
- //- app.use(express.static('client/build'));
+ app.use(express.static('client/build'));
 
   //Express will serve up the index.html file
   //If it doesn't recognize the route
-  //-const path = require('path');
-  //-app.get('*', (req, res) => {
-   //- res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  //-})
-//-}
+  const path = require('path');
+  app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
