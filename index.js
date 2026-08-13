74da1e7ceaa7a029c3a1a5389afcfbb96032e479
nels-frank <frankadzua@gmail.com>
2026-08-13 05:54:08 -0700

    Configure Emailing app for Vercel

--- index.js
+++ index.js
@@ -47,5 +47,13 @@ if (process.env.NODE_ENV === 'production') {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
