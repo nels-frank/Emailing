cf570769dd735362fd07cb9632132e3135eb0050
nels-frank <frankadzua@gmail.com>
2026-08-16 00:50:20 -0700

    Final Vercel Configuration

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
+      //callbackURL: '/auth/google/callback',
+      callbackURL: `${keys.redirectDomain}/auth/google/callback`,
       proxy: true // <- this makes sure the strategy trusts Render's proxy and uses https
     },
 
