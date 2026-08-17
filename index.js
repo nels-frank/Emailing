4895584fc37dca353e4b95b8df98784431bceb68
nels-frank <frankadzua@gmail.com>
2026-08-17 05:17:06 -0700

    Survey Route Back to Index

--- index.js
+++ index.js
@@ -50,9 +50,15 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(
+      path.join(__dirname, 'client', 'build', 'index.html')
+    );
   });
 }
 
