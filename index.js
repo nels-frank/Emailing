da48b7736f010749cbbd308cf07572ea8b6222e7
nels-frank <frankadzua@gmail.com>
2026-08-18 05:36:49 -0700

    Fix production server port

--- index.js
+++ index.js
@@ -49,11 +49,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
